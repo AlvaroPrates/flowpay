ea449b8c9f73e9f0b5278777849d7cb2ed73db8f
Alvaro Prates <alvarobirckprates@gmail.com>
2026-02-04 18:27:18 -0300

    Initial commit

---
+++ src/main/java/com/flowpay/atendimento_service/AtendimentoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flowpay.atendimento_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtendimentoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtendimentoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flowpay/atendimento_service/AtendimentoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowpay.atendimento_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtendimentoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
